9f48792d06ee76ff203e4aa1278a0c6e247c2fe4
Mehu Kumar Sahu <mehul.www.mk@gmail.com>
2026-08-01 20:06:02 +0530

    Increase express body limit to 50mb to fix 413 error

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,8 @@ app.use(async (req, res, next) => {
 
 // Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
